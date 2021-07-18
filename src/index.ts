1191b5422cd2d8d2bb5eaaba645d1f166f2f6eaa
xnought <bertuccd@oregonstate.edu>
2021-07-18 14:30:00 -0700

    add the basic operations without local grads

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,45 @@
+class Value {
+	data: number;
+	constructor(data: number) {
+		this.data = data;
+	}
+	add(other: Value): Value {
+		const output = this.data + other.data;
+		return new Value(output);
+	}
+	subtract(other: Value): Value {
+		const output = this.data - other.data;
+		return new Value(output);
+	}
+	multiply(other: Value): Value {
+		const output = this.data * other.data;
+		return new Value(output);
+	}
+	divide(other: Value): Value {
+		const output = this.data / other.data;
+		return new Value(output);
+	}
+	relu(): Value {
+		const output = this.data > 0 ? this.data : 0;
+		return new Value(output);
+	}
+	print(): void {
+		console.log(`Value=${this.data}`);
+	}
+}
+
 const main = (): void => {
-	console.log("working!");
+	const a = new Value(10);
+	const zero = new Value(0);
+	const b = a.add(a);
+	b.multiply(a)
+		.add(b)
+		.relu()
+		.multiply(zero)
+		.relu()
+		.subtract(a)
+		.divide(a)
+		.print();
 };
 
 main();
