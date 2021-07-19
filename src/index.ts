e17e0fb4d97711ba93a29b0e94acf9780a6ba417
xnought <bertuccd@oregonstate.edu>
2021-07-19 15:29:02 -0700

    create optimizer and sgd

--- src/index.ts
+++ src/index.ts
@@ -7,27 +7,78 @@ class Linear {
 		this.layer = new Layer(numInputs, numNeurons);
 	}
 	forward(X: Value[][]) {
-		let outputs: Value[][] = [];
-		for (const inputs of X) {
-			const output = this.layer.forward(inputs);
-			outputs.push(output);
-		}
+		const outputs = X.map((inputs) => this.layer.forward(inputs));
 		return outputs;
 	}
 	zeroGrad() {
 		this.layer.zeroGrad();
 	}
+	parameters() {
+		return this.layer.parameters();
+	}
+}
+class LinearRegression {
+	l1: Linear;
+	constructor() {
+		this.l1 = new Linear(1, 1);
+	}
+	forward(X: any) {
+		X = this.l1.forward(X);
+		return X;
+	}
+	zeroGrad() {
+		this.l1.zeroGrad();
+	}
+	parameters() {
+		return this.l1.parameters();
+	}
+}
+class MSE {
+	forward(outputs: Value[][], labels: Value[][]): Value {
+		return new Value(0);
+	}
+}
+abstract class Optimizer {
+	parameters: Value[];
+	constructor(parameters: Value[]) {
+		this.parameters = parameters;
+	}
+	abstract step(): void;
+}
+class SGD extends Optimizer {
+	learningRate: number;
+	constructor(parameters: Value[], learningRate: number = 0.001) {
+		super(parameters);
+		this.learningRate = learningRate;
+	}
+	step() {
+		const { learningRate, parameters } = this;
+		for (const value of parameters) {
+			value.data -= learningRate * value.grad;
+		}
+	}
 }
 
+function toValues(arr: number[]) {
+	return arr.map((num) => [new Value(num)]);
+}
+function toNumbers(arr: Value[]) {
+	return arr.map((value) => value.data);
+}
 function main() {
-	const input = [[new Value(1)], [new Value(2)]];
-	const l1 = new Linear(1, 2);
-	const l2 = new Linear(2, 1);
-	console.log(l2.forward(l1.forward(input)));
-	// const l1 = new Layer(1, 2);
-	// const l2 = new Layer(2, 1);
-	// const output = l2.forward(l1.forward(input));
-	// console.log(output);
+	const n = 15;
+	const rangeLinear = (length: number) =>
+		new Array(length).fill(0).map((_, i) => i);
+	const X = rangeLinear(n);
+	const y = rangeLinear(n);
+	const xTrain = toValues(X);
+	const yTrain = toValues(y);
+	console.log("X:", X, "y:", y);
+
+	const loss = new MSE();
+	const model = new LinearRegression();
+	console.log(model.parameters());
+	console.log(model.forward(xTrain));
 }
 
 main();
