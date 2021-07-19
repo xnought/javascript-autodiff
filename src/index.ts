c3004676433114b5a053b709d7f38d4709d65908
xnought <bertuccd@oregonstate.edu>
2021-07-19 14:10:08 -0700

    implmemented Linear

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,33 @@
 import Value from "./Value";
-import Layer from "./Module";
+import Layer, { Module } from "./Module";
+
+class Linear {
+	private layer: Layer;
+	constructor(numInputs: number, numNeurons: number) {
+		this.layer = new Layer(numInputs, numNeurons);
+	}
+	forward(X: Value[][]) {
+		let outputs: Value[][] = [];
+		for (const inputs of X) {
+			const output = this.layer.forward(inputs);
+			outputs.push(output);
+		}
+		return outputs;
+	}
+	zeroGrad() {
+		this.layer.zeroGrad();
+	}
+}
 
 function main() {
-	const input = [new Value(1)];
-	const l1 = new Layer(1, 2);
-	const output = l1.forward(input);
-	console.log(l1.neurons);
-	console.log(output);
+	const input = [[new Value(1)], [new Value(2)]];
+	const l1 = new Linear(1, 2);
+	const l2 = new Linear(2, 1);
+	console.log(l2.forward(l1.forward(input)));
+	// const l1 = new Layer(1, 2);
+	// const l2 = new Layer(2, 1);
+	// const output = l2.forward(l1.forward(input));
+	// console.log(output);
 }
 
 main();
